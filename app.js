5c8259cc5a7714e4ed447a080cfad2aea403d26f
Panayot Angarev <pancho.angarev@gmail.com>
2019-06-24 08:42:13 +0300

    Get input data

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ var budgetController = (function(){
     //Some code
     return {
 
-    }
+    };
 })();
 
 //View
 var UIController = (function(){
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+
     //Some code
     return {
-        
-    }
+      getInput: function () {
+          return {
+            type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+          };
+      },
+      getDOMstrings: function () {
+          return DOMstrings;
+      } 
+    };
 })();
 
 
@@ -27,13 +44,18 @@ var appController = (function(budgetCtrl, UICtrlr){
     //4. Calculate the budget
     //5. Display the budgte on UI
 
+    //Get dom strings from UI controller
+    var DOM = UICtrlr.getDOMstrings();
+
     var ctrlAddItem = function () {
-        
+        var input = UICtrlr.getInput();
+
+        console.log(input);
     }
     
 
     // Add EventListener to add btn when user click with mouse
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     // Add EventListener to add btn when user use return key
     document.addEventListener('keypress', function(event){
